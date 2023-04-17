7962d42c1c357589f1f223c1d2414a092da2d7ef
Jerydam <jerydan148@gmail.com>
2023-04-17 01:35:37 +0100

    well done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Connect from "./Connect";
+import Connect from './Connect';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,18 +9,19 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-wrap items-center justify-between bg-light-blue">
+    <nav className="flex bg-light-blue  flex-wrap">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <a href="">
           <span className="font-bold text-xl text-[#fff] tracking-tight ml-2">
-            <span className="text-5xl text-red">.</span>METASQUARE
+              <span className="text-5xl text-red">.</span>METASQUARE
           </span>
         </a>
       </div>
-      <div className="block lg:hidden">
+      <div className="block lg:hidden ml-auto">
         <button
           className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-500 hover:text-white hover:border-white"
           onClick={toggleMenu}
+          style={{ marginLeft: '10px' }} // added margin to move button towards left
         >
           <svg
             className="fill-current text-white h-3 w-3"
@@ -28,48 +29,45 @@ const Navbar = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            <path
+              d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
+            />
           </svg>
         </button>
       </div>
       <div
         className={`${
           isOpen ? "block" : "hidden"
-        } w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto`}
+        } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}
       >
-        <div className="text-md lg:flex-grow">
+        <div className="text-md ml-[500px] lg:flex-grow">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-white mr-10 sm:ml-[500px] lg:ml-0"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 font-bold hover:text-white mr-10"
           >
             About
           </a>
-          <a
+          <a 
             href="#responsive-header"
-            className="block mt-4 lg:inline-block mr-10 lg:mt-0 text-gray-500 hover:text-white sm:ml-[500px] lg:ml-0"
+            className="block mt-4 lg:inline-block mr-10 font-bold lg:mt-0 text-gray-500 hover:text-white"
           >
             Docs
           </a>
+           <a 
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block mr-10 font-bold lg:mt-0 text-gray-500 hover:text-white"
+          >
+            Devs
+          </a>
           <a
             href="/admin"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 mr-20 hover:text-white sm:ml-[500px] lg:ml-0"
+            className="block mt-4 lg:inline-block font-bold lg:mt-0 text-gray-500 mr-20 hover:text-white"
           >
             Admin
-          </a>
+          </a>  
         </div>
         <Connect />
       </div>
-      <style jsx>{`
-        @media (max-width: 767px) {
-          .flex-col {
-            display: flex;
-            flex-direction: column;
-          }
-          .flex-col .nav-link {
-            margin-top: 10px;
-          }
-        }
-      `}</style>
     </nav>
   );
 };
